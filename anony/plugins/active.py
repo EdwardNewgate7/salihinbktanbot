a708aa1ba249b7366453779284e004f73e04b45c
kececiozgur314-sys <kececiozgur314@gmail.com>
2026-01-21 14:01:38 +0300

    Update active.py

--- anony/plugins/active.py
+++ anony/plugins/active.py
@@ -10,13 +10,15 @@ from pyrogram import filters, types
 from anony import app, db, lang, queue
 
 
-@app.on_message(filters.command(["ac", "activevc"]) & app.sudoers)
+@app.on_message(filters.command(["ac", "av", "activevc", "activevoice", "vc"]) & ~app.bl_users)
 @lang.language()
 async def _activevc(_, m: types.Message):
+    if m.from_user.id not in app.sudoers:
+        return await m.reply_text(m.lang.get("sudo_only", "This command is only for sudo users."))
     if not db.active_calls:
         return await m.reply_text(m.lang["vc_empty"])
 
-    if m.command[0] == "ac":
+    if m.command[0] in ["ac", "av"]:
         return await m.reply_text(m.lang["vc_count"].format(len(db.active_calls)))
 
     sent = await m.reply_text(m.lang["vc_fetching"])
